035bdac17cd879c8733dd346249de01cb8a9f8ea
Khavin K K <khavinkrishnan@gmail.com>
2024-06-29 20:15:43 -0400

    added custom hook for getting window dimensions

---
+++ client/src/hooks/windowDimension.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+
+// Get current dimensions
+function getWindowDimension() {
+  const h = window.innerHeight;
+  const w = window.innerWidth;
+
+  return {
+    w,
+    h,
+  };
+}
+
+// This is a custom hook, which can be used to get the current dimensions of the window
+// More info refer - https://stackoverflow.com/questions/36862334/get-viewport-window-height-in-reactjs/36862446#36862446
+export function useWindowDimensions() {
+  const [dimension, updateDimension] = useState(getWindowDimension());
+  useEffect(() => {
+    function handleResize() {
+      updateDimension(getWindowDimension);
+    }
+    window.addEventListener("resize", handleResize);
+
+    // Clean up function
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return dimension;
+}
