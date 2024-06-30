68a044278f63cc9fcf9e407b52322e8775aa52b2
Khavin K K <khavinkrishnan@gmail.com>
2024-06-30 19:53:45 -0400

    added comments for mobile menu

--- client/src/menu.jsx
+++ client/src/menu.jsx
@@ -222,6 +222,10 @@ export function Menu() {
 }
 
 function MobileMenu() {
+  // This state is used as a stack to push array indices.
+  // The array indices belong to the children array in data.
+  // The top value of the stack is the screen to render. If the
+  // stack is empty, then display the main menu.
   const [state, setState] = useState([]);
 
   let itemsToDisplay = data;
@@ -229,6 +233,7 @@ function MobileMenu() {
     itemsToDisplay = itemsToDisplay["children"][index];
   });
 
+  // Use this const to identify whether the user is in the first menu screen or not
   const isMainMenu = state.length === 0;
 
   const elements = itemsToDisplay["children"].map((item, index) => {
@@ -236,9 +241,11 @@ function MobileMenu() {
       <li
         className={isMainMenu ? classes["main-menu-item"] : ""}
         key={item.label}
+        // Add onClick event listener if the nav item is a button
         onClick={
           item.children
             ? () => {
+                // Add new index to the stack to move to the new screen
                 const newState = [...state];
                 newState.push(index);
                 setState(newState);
@@ -246,9 +253,11 @@ function MobileMenu() {
             : undefined
         }
       >
+        {/* Render logos if there are any */}
         {item.image && (
           <img className={classes["menu-logo"]} src={item.image} />
         )}
+        {/* There are two types of navigation: links and buttons */}
         {item.children ? (
           <button
             className={
@@ -280,10 +289,12 @@ function MobileMenu() {
 
   return (
     <nav>
+      {/* Previous screen navigation */}
       {itemsToDisplay.label && (
         <button
           className={classes["menu-button"] + " " + classes["previous-menu"]}
           onClick={() => {
+            // Remove the top element of the stack to move to the previous screen
             const newState = [...state];
             newState.pop();
             setState(newState);
@@ -292,6 +303,7 @@ function MobileMenu() {
           <img src={LeftNavURL} /> {itemsToDisplay.label}
         </button>
       )}
+      {/* All content link */}
       {itemsToDisplay.href && (
         <a
           className={classes["menu-link"] + " " + classes["all-content-link"]}
@@ -300,6 +312,7 @@ function MobileMenu() {
           All {itemsToDisplay.label}
         </a>
       )}
+      {/* Next screen navigation */}
       <ul className={classes["individual-content-links"]}>{elements}</ul>
     </nav>
   );
