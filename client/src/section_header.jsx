60d535e043ee497e9148b6f134d5f8a97f133751
Khavin K K <khavinkrishnan@gmail.com>
2024-07-06 21:30:53 -0400

    fixed styling issue for section header

--- client/src/section_header.jsx
+++ client/src/section_header.jsx
@@ -2,7 +2,11 @@ import classes from "./section_header.module.css";
 
 export function SectionHeader({ title, imageLink }) {
   return (
-    <h2 className={classes["title"]}>
+    <h2
+      className={
+        classes["title"] + " " + (imageLink && classes["title-with-img"])
+      }
+    >
       {title}
       {imageLink && <img src={imageLink}></img>}
     </h2>
