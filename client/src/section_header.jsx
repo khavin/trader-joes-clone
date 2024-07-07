150f09d9f6a3c5bce1e217afa581b12e9cf963e5
Khavin K K <khavinkrishnan@gmail.com>
2024-07-06 21:23:42 -0400

    fixed encapsulaiton issue with section header

--- client/src/section_header.jsx
+++ client/src/section_header.jsx
@@ -2,7 +2,7 @@ import classes from "./section_header.module.css";
 
 export function SectionHeader({ title, imageLink }) {
   return (
-    <h2>
+    <h2 className={classes["title"]}>
       {title}
       {imageLink && <img src={imageLink}></img>}
     </h2>
