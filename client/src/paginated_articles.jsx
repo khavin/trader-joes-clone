052126081c257f0d40d39a8612d8c8b77c994237
Khavin K K <khavinkrishnan@gmail.com>
2024-07-08 13:23:15 -0400

    fixed tab navigation issue in whats new section

--- client/src/paginated_articles.jsx
+++ client/src/paginated_articles.jsx
@@ -8,15 +8,19 @@ const MOBILE_WIDTH = 900;
 export function PaginatedArticles({ articles }) {
   const componentId = useId();
   const [page, setPage] = useState(0);
+  // Current window width
   const { width } = useWindowDimensions();
 
   function setPageAndScroll(nextPage) {
+    // Set page number
     setPage(nextPage);
+    // Scroll to the corresponding element id
     document
       .getElementById(componentId + "-article-" + nextPage)
       .scrollIntoView(true);
   }
 
+  // Create article elements
   const elements = articles.map((article, index) => {
     return (
       <article
@@ -36,13 +40,17 @@ export function PaginatedArticles({ articles }) {
           })}
         </picture>
         <h3 className={classes["title"]}>
-          <a href={article.link}>{article.title}</a>
+          {/* Set tab index for the article element in view. This will disable scrolling to next/previous article using tab. Users should be able to navigate to other articles only using pagination. */}
+          <a tabIndex={page === index ? "0" : "-1"} href={article.link}>
+            {article.title}
+          </a>
         </h3>
         <p className={classes["content"]}>{article.content}</p>
       </article>
     );
   });
 
+  // We only want to show the pagination for desktop screens
   const numberPagination =
     width > MOBILE_WIDTH ? (
       articles.map((_, index) => {
@@ -63,6 +71,7 @@ export function PaginatedArticles({ articles }) {
         );
       })
     ) : (
+      // The presentation bullets will be shown for the mobile screens
       <div className={classes["bullets"]} role="presentation">
         <span></span>
         <span></span>
