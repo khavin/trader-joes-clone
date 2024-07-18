44e2514e44b72379c987cf0e0eab13cc2e19ba62
Khavin K K <khavinkrishnan@gmail.com>
2024-07-18 14:26:23 -0400

    completed shopping list page

--- client/src/shopping_list.jsx
+++ client/src/shopping_list.jsx
@@ -3,6 +3,10 @@ import { useLocation } from "wouter";
 import { ShoppingListContext } from "./contexts/ShoppingListContext";
 import ContainerTop from "./assets/empty_container_top.webp";
 import ContainerBottom from "./assets/empty_container_bottom.webp";
+import ProductsURL from "./assets/products.webp";
+import PlusURL from "./assets/plus.svg";
+import MinusURL from "./assets/minus.svg";
+import DustBinURL from "./assets/dustbin.svg";
 import classes from "./shopping_list.module.css";
 
 export function ShoppingList() {
@@ -35,12 +39,47 @@ export function ShoppingList() {
     );
   }
 
+  function incrementQuantity(item) {
+    const clone = structuredClone(shoppingList);
+    clone[item].quantity++;
+    setShoppingList(clone);
+  }
+
+  function decrementQuantity(item) {
+    const clone = structuredClone(shoppingList);
+    if (clone[item].quantity === 1) {
+      delete clone[item];
+    } else {
+      clone[item].quantity--;
+    }
+
+    setShoppingList(clone);
+  }
+
+  function deleteItem(item) {
+    const clone = structuredClone(shoppingList);
+    if (item in clone) {
+      delete clone[item];
+    }
+    setShoppingList(clone);
+  }
+
+  function clearAll() {
+    setShoppingList({});
+  }
+
   return (
-    <main className={classes["container"]}>
+    <main
+      className={
+        classes["container"] + " " + classes["container-with-shopping-items"]
+      }
+    >
       <div>
         <div className={classes["heading"]}>
           <h2 className={classes["title"]}>Shopping List</h2>
-          <button className={classes["clear-all-button"]}>Clear all</button>
+          <button onClick={clearAll} className={classes["clear-all-button"]}>
+            Clear all
+          </button>
         </div>
         <ul className={classes["list"]}>
           {Object.keys(shoppingList).map((item) => {
@@ -52,18 +91,56 @@ export function ShoppingList() {
                     src={shoppingList[item].imgURL}
                   />
                 </picture>
-                <p className={classes["item-info"]}>
-                  <span className={classes["item-name"]}>{item}</span>
-                  <span className={classes["item-weight"]}>
-                    {shoppingList[item].perUnit}
-                  </span>
-                </p>
+                <div className={classes["info-and-action-buttons-container"]}>
+                  <p className={classes["item-info"]}>
+                    <span className={classes["item-name"]}>{item}</span>
+                    <span className={classes["item-weight"]}>
+                      {shoppingList[item].perUnit}
+                    </span>
+                  </p>
+                  <p className={classes["action-buttons"]}>
+                    <span className={classes["quantity-action-buttons"]}>
+                      <button
+                        onClick={() => decrementQuantity(item)}
+                        className={classes["action-button"]}
+                      >
+                        <img src={MinusURL} />
+                      </button>
+                      <span className={classes["quantity-number"]}>
+                        {shoppingList[item].quantity}
+                      </span>
+                      <button
+                        onClick={() => incrementQuantity(item)}
+                        className={classes["action-button"]}
+                      >
+                        <img src={PlusURL} />
+                      </button>
+                    </span>
+
+                    <button
+                      onClick={() => deleteItem(item)}
+                      className={classes["action-button"]}
+                    >
+                      <img src={DustBinURL} />
+                    </button>
+                  </p>
+                </div>
               </li>
             );
           })}
         </ul>
       </div>
-      <aside></aside>
+      <aside>
+        <article className={classes["about-list-container"]}>
+          <img className={classes["products-logo"]} src={ProductsURL} />
+          <h3>This shopping list is just a list</h3>
+          <p className={classes["about-list-text"]}>
+            We do not sell any products online. We set up our stores with care,
+            filling them with knowledgeable, welcoming Crew Members and a sense
+            of adventure.
+          </p>
+        </article>
+      </aside>
     </main>
   );
 }
